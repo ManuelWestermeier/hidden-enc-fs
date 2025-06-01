76078634cb4833fac304eb138f747602369462a7
ManuelWestermeier <westermeier111@gmail.com>
2025-06-01 22:33:13 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "docs" },
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'index.css';
+          }
+          return '[name].[ext]';
+        },
+      },
+    },
+  },
   server: {
     port: 26270,
   }
